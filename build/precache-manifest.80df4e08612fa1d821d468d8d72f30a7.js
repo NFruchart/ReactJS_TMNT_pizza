b71e6d42e26340d5d11a9fe67eda1d8e2461b3ad
NFruchart <nicolasfruchart@gmail.com>
2019-11-07 00:25:24 +0100

    Initial commit

---
+++ build/precache-manifest.80df4e08612fa1d821d468d8d72f30a7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af38c61758d7ab38e355fd40f074f5c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44f1edb6d6814779be81",
+    "url": "/static/css/main.7357df9c.chunk.css"
+  },
+  {
+    "revision": "34ec71f663a1d50bc468",
+    "url": "/static/js/2.b0468b57.chunk.js"
+  },
+  {
+    "revision": "44f1edb6d6814779be81",
+    "url": "/static/js/main.d94fa6fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bf16a90d3813b54a408e0ee1a7b08932",
+    "url": "/static/media/4-fromages.bf16a90d.jpg"
+  },
+  {
+    "revision": "44ae2bd2cbfb0326a5436cba09c9bd13",
+    "url": "/static/media/marguerita.44ae2bd2.jpg"
+  },
+  {
+    "revision": "b9c11f755f49f37819c355641cf2bc2f",
+    "url": "/static/media/pepperoni.b9c11f75.jpg"
+  },
+  {
+    "revision": "f4fde97f4716891f622d7aa2b6e80f02",
+    "url": "/static/media/reine.f4fde97f.jpg"
+  },
+  {
+    "revision": "3feb5ffe6960678952ebcaa1cc980ed8",
+    "url": "/static/media/turtle.3feb5ffe.jpg"
+  }
+]);
